215daedbbae254d4c4ea1df4930088ebbccb73b9
mike <mike@espirus.com>
2021-06-17 15:09:42 +0100

    Update app.tsx

--- app.tsx
+++ app.tsx
@@ -103,7 +103,7 @@ function ProductItem({ hit, insights, components }: ProductItemProps) {
     <a href={hit.url} className="aa-ItemLink">
       <div className="aa-ItemContent">
         <div className="aa-ItemIcon aa-ItemIcon--picture aa-ItemIcon--alignTop">
-          <img src={hit.image} alt={hit.name} width="40" height="40" />
+          <img src={hit.image} alt={hit.name} width="80" height="80" />
         </div>
         <div className="aa-ItemContentBody">
           <div className="aa-ItemContentTitle">
@@ -150,7 +150,7 @@ function ProductItem({ hit, insights, components }: ProductItemProps) {
           title="Select"
           style={{ pointerEvents: 'none' }}
         >
-          <svg viewBox="0 0 24 24" width="40" height="40" fill="currentColor">
+          <svg viewBox="0 0 24 24" width="20" height="20" fill="currentColor">
             <path d="M18.984 6.984h2.016v6h-15.188l3.609 3.609-1.406 1.406-6-6 6-6 1.406 1.406-3.609 3.609h13.172v-4.031z" />
           </svg>
         </button>
