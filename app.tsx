050e4cc41ec7554b7c9eb998d0c1d4ce4020e6a4
mike <mike@espirus.com>
2021-06-17 14:54:43 +0100

    Update app.tsx

--- app.tsx
+++ app.tsx
@@ -122,8 +122,8 @@ function ProductItem({ hit, insights, components }: ProductItemProps) {
                   return (
                     <svg
                       key={index}
-                      width="16"
-                      height="16"
+                      width="26"
+                      height="26"
                       viewBox="0 0 24 24"
                       fill={isFilled ? 'currentColor' : 'none'}
                       stroke="currentColor"
